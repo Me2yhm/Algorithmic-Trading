861c2117632a3e62233d90cae3beb31d42f4aba8
miaozhipeng09 <139319633+miaozhipeng09@users.noreply.github.com>
2023-08-01 16:51:22 +0800

    Add files via upload

--- AlgorithmicStrategy/TWAP_VWAP/BACKUP/TWAP.py
+++ AlgorithmicStrategy/TWAP_VWAP/BACKUP/TWAP.py
@@ -1,26 +1,31 @@
 import time
 
-def twap_trading(total_volume, num_intervals):
-    interval_volume = total_volume / num_intervals
+class TWAPTradingSystem:
+    def __init__(self, total_volume, num_intervals, interval_duration=300):
+        self.total_volume = total_volume
+        self.num_intervals = num_intervals
+        self.interval_duration = interval_duration
 
+    def execute_trade(self, volume):
+        # 在这里执行交易操作，输出交易份额
+        print(f"执行交易，交易份额为: {volume}")
 
-    start_time = time.time()
-    end_time = start_time + 4 * 60 * 60  # 4个小时，单位为秒
+    def twap_trading(self):
+        interval_volume = self.total_volume / self.num_intervals
 
+        # 设置交易开始时间和结束时间
+        start_time = time.time()
+        end_time = start_time + 4 * 60 * 60  # 4个小时，单位为秒
 
-    interval_duration = 5 * 60  # 5分钟，单位为秒
+        while time.time() < end_time:
+            # 执行交易，输出交易份额
+            self.execute_trade(interval_volume)
 
-    while time.time() < end_time:
-
-        execute_trade(interval_volume)
-
-        time.sleep(interval_duration)
-
-def execute_trade(volume):
-    # 在这里执行交易操作，输出交易份额
-    print(f"执行交易，交易份额为: {volume}")
+            # 等待指定的时间间隔
+            time.sleep(self.interval_duration)
 
 if __name__ == "__main__":
     total_trade_volume = 10000  # 总交易量
     num_intervals = 48  # 总交易时间为4个小时，每5分钟输出一次交易份额，共48个时间间隔
-    twap_trading(total_trade_volume, num_intervals)
+    twap_system = TWAPTradingSystem(total_trade_volume, num_intervals)
+    twap_system.twap_trading()
