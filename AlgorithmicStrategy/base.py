bf1cb38327f69659a5abfb8982e57af8fd087029
echo Yang <Yhmecnu@163.com>
2023-08-03 14:43:37 +0800

    refactor: (base.py)(main.py) refactor and add some attribute

--- AlgorithmicStrategy/base.py
+++ AlgorithmicStrategy/base.py
@@ -1,53 +1,89 @@
 from abc import ABC, abstractmethod
 
-from pandas import DataFrame
 from typing import Dict, List
-
-
-class OrderBook(ABC):
-    pass
+from TWAP_VWAP.OrderMaster.OrderBook import OrderBook
 
 
 class AlgorithmicStrategy(ABC):
     """
     抽象基类, 定义算法交易类的接口, 有如下属性
     orderbook: orderbook类, 可以撮合盘口, 记录盘口状态
-    timeStamp: 记录当前时间戳
-    deal: 成交记录
-    possession: 调仓记录
+    timeStamp: 记录当前时间戳和上一秒时间戳
+    deals: \成交记录,一个由字典组成的列表,
+          \字典的形式为: {timestamp:int,symbol:str,direction:str,price:float,volume:int}
+    possession: \持仓记录,一个字典
+                \字典形式为{code:str,volume:int,averagePrice:float,cost:float}
+                \其中cost是总的金额, averagePrice是总金额除以总量
+    signal: \交易信号, 由字典组成的列表
+            \字典形式为{symbol:str,direction:str,price:float,volume:int}
+            \symbol为股票代码, direction表示买入或者卖出: ["B"/"S"]
+    tick: \逐笔数据, 由字典组成的列表
+          \字典形式为: {time:int,oid:int,oidb:int,oids:int,price:float,volume:int,flag:int,ptype:int}
+          \ time表示当前时间戳, oid等键名的含义可以查询: 语雀|技术团队|数据格式知识库|逐笔数据文档
+    commission: 手续费, 券商收取, 默认按万分之1.5算
+    stamp_duty: 印花税, 买入没有印花税, 卖出有, 为0.001
+    transfer_fee: 过户费, 为0.00002, 买卖都有
     """
 
-    # orderbook类, 还没定义
     orderbook: OrderBook
     tick: List[Dict]
     signal: List[Dict]
     timeStamp: int
-    deal: DataFrame
-    possession: DataFrame
+    deal: List[Dict]
+    possession: Dict
+    commission: float
+    stamp_duty: float
+    transfer_fee: float
+    ask_cost: float
+    bid_cost: float
 
-    @abstractmethod
-    def update_orderbook(self) -> dict:
+    def __init__(
+        self,
+        commision: float = 0.00015,
+        stamp_duty: float = 0.001,
+        transfer_fee: float = 0.00002,
+    ) -> None:
+        self.tick = []
+        self.signal = []
+        self.timeStamp = 0
+        self.deals = []
+        self.possession = {"code": "", "volume": 0, "averagePrice": 0.0, "cost": 0.0}
+        self.commission = commision
+        self.stamp_duty = stamp_duty
+        self.transfer_fee = transfer_fee
+        self.buy_cost = self.commission + self.transfer_fee
+        self.sell_cost = self.commission + self.transfer_fee + self.stamp_duty
+
+    def update_orderbook(self, lines: List[Dict]) -> None:
         """
         更新orderbook和tick数据
-
         """
+        self.tick.extend(lines)
+        try:
+            assert all([line["timestamp"] for line in lines])
+            self.timeStamp = lines[-1]["timestamp"]
+            self.orderbook.single_update(lines)
+        except AssertionError:
+            raise ValueError("lines need the same timestamp")
 
     @abstractmethod
     def model_update(self) -> None:
         """
-        更新模型，数据流式传入，对模型进行流式训练和更新
+        更新模型, 数据流式传入, 对模型进行流式训练和更新
         """
 
     @abstractmethod
-    def signal_update(self) -> Dict:
+    def signal_update(self) -> None:
         """
-        模型更新之后，根据模型训练结果更新signal，
-        signal是一个元素为字典的列表，每个signal包含{股票代码, 买入还是卖出["B"/"S"], 价格, volume}
+        调用model_update函数, 根据模型训练结果更新signal,
+        signal是一个元素为字典的列表, 每个signal包含{股票代码, 买入还是卖出["B"/"S"], 价格, volume}
+        signal: {symbol:str,direction:str,price:float,volume:int}
+
         """
 
     @abstractmethod
     def stratgy_update(self) -> Dict:
         """
-        根据更新过后的signal，更新成交记录和持仓记录，更新策略的评价结果
-        动量是胜率、赔率，VWAP是成交成本与实际VWAP的差
+        根据更新过后的signal, 更新成交记录和持仓记录, 更新策略的评价结果
+        动量是胜率、赔率, VWAP是成交成本与实际VWAP的差
         """
--- AlgorithmicStrategy/momentum_stratgy/main.py
+++ AlgorithmicStrategy/momentum_stratgy/main.py
@@ -1,23 +1,6 @@
-from abc import ABC, abstractmethod
-from pathlib import Path
 from AlgorithmicStrategy.base import AlgorithmicStrategy
-from ..TWAP_VWAP.OrderMaster.DataManager import OT, DataSet
-from ..TWAP_VWAP.OrderMaster.OrderBook import OrderBook
+from .modelType import modelType
 from typing import List, Dict
-from pandas import DataFrame
-
-
-class modelType(ABC):
-    """
-    模型的抽象基类，用于实现指标更新或者模型增量训练
-
-    """
-
-    @abstractmethod
-    def model_update(tick) -> dict[str, float]:
-        """
-        模型接受一个tick参数(目前还不确定是什么数据类型)，根据tick数据返回模型计算结果
-        """
 
 
 class momentumStratgy(AlgorithmicStrategy):
@@ -26,41 +9,78 @@ class momentumStratgy(AlgorithmicStrategy):
     orderbook: orderbook类, 可以撮合盘口, 记录盘口状态
     tick: 储存逐笔数据
     timeStamp: 记录当前时间戳
-    deal: 成交记录
+    deals: 成交记录
     possession: 调仓记录
     model_indicator: 指标计算结果
+    signal:记录交易信号
+    win_times: 一个列表, 记录每笔交易盈利与否, 盈利为1, 否则为0
+    win_probability: 胜率
+    odds: 赔率
     """
 
-    orderbook: OrderBook
-    tick: List[Dict]
-    timeStamp: int
-    deal: List[Dict]
-    possession: List[Dict]
-    model_indicator: Dict[str, float]
+    win_times: List[int]
+    model_indicator: List[Dict[str, float]]
+    win_probability = float
+    odds: float
 
-    def update_oderbook(self, symbol, datestr) -> None:
-        """
-        流式读取数据，更新盘口
-        """
-        data_api = (
-            Path(__file__).parent / f"../../datas/{symbol}/tick/gtja/{datestr}.csv"
-        )
-        tick = DataSet(data_api, date_column="time", ticker=symbol)
+    def __init__(self) -> None:
+        super().__init__()
+        self.model_indicator = []
+        self.win_times = []
+        self.win_probability = 0
+        self.odds = 0
 
     def model_update(self, model: modelType) -> None:
         """
-        盘口更新过后，根据更新过的数据增量地更新指标或者训练模型
+        盘口更新过后, 根据更新过的数据增量地更新指标或者训练模型
         """
         pass
 
     def signal_update(self) -> dict:
         """
-        调用model_update函数，根据函数结果返回信号
+        调用model_update函数, 根据函数结果返回信号
         """
         pass
 
-    def exam_update(self) -> dict:
+    def update_deal(self) -> None:
+        self.deals.append(self.signal[-1])
+
+    def update_poccession(self) -> None:
+        deal = self.deals[-1]
+        money = deal["volume"] * deal["price"]
+        buy_commission = self.buy_cost * money
+        sell_commission = self.sell_cost * money
+        total = self.possession["volume"] * self.possession["averagePrice"]
+
+        if self.possession["code"] == "":
+            self.possession["code"] = deal["symbol"]
+
+        if deal["direction"] == "B":
+            self.possession["volume"] += deal["volume"]
+            self.possession["cost"] += money + buy_commission
+            self.possession["averagePrice"] = (total + money) / self.possession[
+                "volume"
+            ]
+            if deal["price"] < self.possession["averagePrice"]:
+                self.win_times.append(1)
+            else:
+                self.win_times.append(0)
+        else:
+            self.possession["volume"] -= deal["volume"]
+            self.possession["cost"] -= money + sell_commission
+            self.possession["averagePrice"] = (total - money) / self.possession[
+                "volume"
+            ]
+            if deal["price"] > self.possession["averagePrice"]:
+                self.win_times.append(1)
+            else:
+                self.win_times.append(0)
+
+    def stratgy_update(self) -> Dict:
         """
         根据返回的信号计算胜率、赔率、换手率等——可以流式？
         """
-        pass
+        self.signal_update()
+        self.update_deal()
+        self.update_poccession()
+        self.win_probability = sum(self.win_times) / len(self.win_times)
