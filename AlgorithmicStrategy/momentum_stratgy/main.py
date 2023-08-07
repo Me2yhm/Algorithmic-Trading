4f66810f28d61add2e5a941da79b241e7240e785
echo Yang <Yhmecnu@163.com>
2023-08-07 15:55:41 +0800

    refactor

--- AlgorithmicStrategy/momentum_stratgy/main.py
+++ AlgorithmicStrategy/momentum_stratgy/main.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import List, Dict
-from AlgorithmicStrategy.base import AlgorithmicStrategy, possession, signal
+from ..base import AlgorithmicStrategy, possession, signal
 from .modelType import modelType
 
 
