1a57803521c3b86702b60edebb09e05270728c12
Alkaid <1176671796@qq.com>
2023-08-17 17:19:14 +0800

    (refactor)ordermaster

---
+++ AlgorithmicStrategy/OrderMaster/Normalized_reader.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import os
+from collections import OrderedDict
+
+
+class Normalized_reader:
+    def __init__(self, filepath) -> None:
+        self.filepath = filepath
+        self.filenames = []
+        self.df_total = dict()
+        self.df_input = OrderedDict()
+        self.trade_record = OrderedDict()
+        self.timestamp_list = []
+        for filename in os.listdir(self.filepath):
+            if filename.endswith(".csv"):
+                self.filenames.append(filename)
+                self.df_total[filename] = pd.read_csv(self.filepath + filename)
+
+    def get_df_input(self, filename):
+        self.trade_record = OrderedDict()
+        self.df_input = OrderedDict()
+        for limit in range(100, len(self.df_total[filename])):
+            trade_price = self.df_total[filename].iloc[limit, 1]
+            trade_time = self.df_total[filename].iloc[limit, 0]
+            self.trade_record[trade_time] = {
+                "trade_volume": None,
+                "trade_price": trade_price,
+            }
+            self.df_input[trade_time] = self.df_total[filename].iloc[
+                limit - 100 : limit, 2:-2
+            ]
+
+        self.timestamp_list = list(self.df_input.keys())
+
+        return self.df_input, self.trade_record
--- AlgorithmicStrategy/OrderMaster/__init__.py
+++ AlgorithmicStrategy/OrderMaster/__init__.py
@@ -5,3 +5,6 @@ from .DataManager import DataSet, DataStream, DataBase
 from .OrderBook import OrderBook
 from .Schema import OrderTick, LifeTime, SnapShot, OrderFlag, OrderDepth, Excecuted_trade
 from .Writer import Writer
+from .Depth import *
+from .Normalized_reader import *
+from .Normalizer import *
