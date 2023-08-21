35791b0e2d14281abdf0d1d4c02b20d636975e5b
bsy <1844112408@qq.com>
2023-08-21 17:56:00 +0800

    snapshot

--- AlgorithmicStrategy/SnapShot/model_1.py
+++ AlgorithmicStrategy/SnapShot/model_1.py
@@ -3,6 +3,7 @@ import datetime
 import json
 from pylab import mpl
 import csv
+from typing import Dict
 from urllib.request import urlopen, quote
 import numpy as np
 import pandas as pd
@@ -13,6 +14,7 @@ import math
 from scipy.stats import poisson
 import random
 from abc import ABC, abstractmethod
+from ..base import possession,signal,deal
 
 random.seed(100)
 
@@ -61,8 +63,8 @@ class TradingSimulator(ABC):
         self.n_paths = 500
         self.h = 6 * 60
         self.adverse = False
-        self.positions = {}  # 持仓记录
-        self.transactions = []  # 成交记录
+        self.signals = []
+        
 
         if model == 'AS':
             self.deltaBidZero = 0.5 * self.gamma * self.sigma**2 * (
@@ -131,188 +133,235 @@ class TradingSimulator(ABC):
 
         return 1 / (1 + math.exp(-x))
     
-    @abstractmethod
-    def model_update(self,predictions) -> None:
-        """
-        模型接受一个self参数，根据self数据返回模型计算结果
-        """
-
-        # 根据模型预测结果生成信号
-        signals = []
-        for prediction in predictions:
-            # 根据预测结果生成信号
-            signal = {
-                "symbol": "AAPL",  # 股票代码
-                "direction": "B" if random.random() < prediction else "S",  # 买入还是卖出 
-                "price": 100.0,  # 价格
-                "volume": 100  # 交易量
-            }
-            signals.append(signal)
-
-        return signals
-         
-    
-    @abstractmethod
-    def signal_update(self, signals) -> None:
-        """
-        调用model_update函数, 根据模型训练结果更新signal,
-        signal是一个元素为字典的列表, 每个signal包含{股票代码, 买入还是卖出["B"/"S"], 价格, volume}
-        signal: {symbol:str,direction:str,price:float,volume:int}
-        """
-        for signal in signals:
-            symbol = signal["symbol"]
-            direction = signal["direction"]
-            price = signal["price"]
-            volume = signal["volume"]
-
-            # 更新成交记录
-            transaction = {
-                "symbol": symbol,
-                "direction": direction,
-                "price": price,
-                "volume": volume
-            }
-            self.transactions.append(transaction)
-
-            # 更新持仓记录
-            if symbol in self.positions:
-                self.positions[symbol] += volume if direction == "B" else -volume
-            else:
-                self.positions[symbol] = volume if direction == "B" else -volume
-
-    @abstractmethod
-    def strategy_update(self) -> None:
-        """
-        根据更新过后的signal, 更新成交记录和持仓记录, 更新策略的评价结果
-        评价指标是收益率
-        """
-        total_profit = 0.0
-        for transaction in self.transactions:
-            symbol = transaction["symbol"]
-            direction = transaction["direction"]
-            price = transaction["price"]
-            volume = transaction["volume"]
-
-            # 根据成交记录计算收益率
-            profit = (price - self.positions[symbol]) * volume if direction == "B" else (self.positions[symbol] - price) * volume
-            total_profit += profit
-
-        return total_profit
 
 
     def execute(self):
-        # time = self.data['time'].values
-        prices = self.prices
-        # bid_prices = self.data[['bid1_price', 'bid2_price', 'bid3_price', 'bid4_price', 'bid5_price']].values
-        # bid_volumes = self.data[['bid1_volume', 'bid2_volume', 'bid3_volume', 'bid4_volume', 'bid5_volume']].values
-        # ask_prices = self.data[['ask1_price', 'ask2_price', 'ask3_price', 'ask4_price', 'ask5_price']].values
-        # ask_volumes = self.data[['ask1_volume', 'ask2_volume', 'ask3_volume', 'ask4_volume', 'ask5_volume']].values
 
-        orders = np.zeros(self.n_steps)
-        orders[0] = 0
+        def model_update(self):
+            # time = self.data['time'].values
+            prices = self.prices
+            # bid_prices = self.data[['bid1_price', 'bid2_price', 'bid3_price', 'bid4_price', 'bid5_price']].values
+            # bid_volumes = self.data[['bid1_volume', 'bid2_volume', 'bid3_volume', 'bid4_volume', 'bid5_volume']].values
+            # ask_prices = self.data[['ask1_price', 'ask2_price', 'ask3_price', 'ask4_price', 'ask5_price']].values
+            # ask_volumes = self.data[['ask1_volume', 'ask2_volume', 'ask3_volume', 'ask4_volume', 'ask5_volume']].values
 
-        cashflow = np.zeros(self.n_steps)
-        cashflow[0] = 0
+            orders = np.zeros(self.n_steps)
+            orders[0] = 0
 
-        # Create the delta bid and asks and add the first value
-        deltaBid = np.zeros(self.n_steps)
-        deltaAsk = np.zeros(self.n_steps)
-        deltaBid[0] = self.deltaBidZero
-        deltaAsk[0] = self.deltaAskZero
+            cashflow = np.zeros(self.n_steps)
+            cashflow[0] = 0
 
-        lambdaBid = np.zeros(self.n_steps)
-        lambdaAsk = np.zeros(self.n_steps)
-        lambdaBid[0] = self.A * math.exp(-self.k * self.deltaBidZero)
-        lambdaAsk[0] = self.A * math.exp(-self.k * self.deltaAskZero)
-        pnl_of_simulation = np.zeros((self.number))
-        final_inventory = np.zeros((self.number))
+            # Create the delta bid and asks and add the first value
+            deltaBid = np.zeros(self.n_steps)
+            deltaAsk = np.zeros(self.n_steps)
+            deltaBid[0] = self.deltaBidZero
+            deltaAsk[0] = self.deltaAskZero
 
-        for t in range(1, self.n_steps - 3):
+            lambdaBid = np.zeros(self.n_steps)
+            lambdaAsk = np.zeros(self.n_steps)
+            lambdaBid[0] = self.A * math.exp(-self.k * self.deltaBidZero)
+            lambdaAsk[0] = self.A * math.exp(-self.k * self.deltaAskZero)
+            pnl_of_simulation = np.zeros((self.number))
+            final_inventory = np.zeros((self.number))
 
-            # If the model we are using the AS model then we set the lambda and poisson process values
-            if self.model == 'AS':
-                deltaBid[t] = round(
-                    max(
-                        0.5 * self.gamma * self.sigma**2 *
-                        (self.T[-1] - self.T[0]) +
-                        1 / self.gamma * math.log(1 + self.gamma / self.k) +
-                        self.gamma * self.sigma**2 *
-                        (self.T[-1] - self.T[0]) * orders[t - 1], 0), 2)
-                deltaAsk[t] = round(
-                    max(
-                        0.5 * self.gamma * self.sigma**2 *
-                        (self.T[-1] - self.T[0]) +
-                        1 / self.gamma * math.log(1 + self.gamma / self.k) -
-                        self.gamma * self.sigma**2 *
-                        (self.T[-1] - self.T[0]) * orders[t - 1], 0), 2)
+            for t in range(1, self.n_steps - 3):
 
-            # Else we proceed with the initial delta values
+                # If the model we are using the AS model then we set the lambda and poisson process values
+                if self.model == 'AS':
+                    deltaBid[t] = round(
+                        max(
+                            0.5 * self.gamma * self.sigma**2 *
+                            (self.T[-1] - self.T[0]) +
+                            1 / self.gamma * math.log(1 + self.gamma / self.k) +
+                            self.gamma * self.sigma**2 *
+                            (self.T[-1] - self.T[0]) * orders[t - 1], 0), 2)
+                    deltaAsk[t] = round(
+                        max(
+                            0.5 * self.gamma * self.sigma**2 *
+                            (self.T[-1] - self.T[0]) +
+                            1 / self.gamma * math.log(1 + self.gamma / self.k) -
+                            self.gamma * self.sigma**2 *
+                            (self.T[-1] - self.T[0]) * orders[t - 1], 0), 2)
+
+                # Else we proceed with the initial delta values
+                else:
+                    deltaBid[t] = self.deltaBidZero
+                    deltaAsk[t] = self.deltaAskZero
+
+            for simulation in range(self.number):
+                pnl = np.zeros((len(prices)))
+                cashflow = np.zeros((len(prices)))
+                orders = np.zeros((len(prices)))
+                reserve_price = np.zeros((len(prices)))
+                r_optimal_ask = np.zeros((len(prices)))
+                r_optimal_bid = np.zeros((len(prices)))
+
+                for step in range(len(prices) - 1):
+                    reserve_price[step] = prices[
+                        step] - orders[step] * self.gamma * (self.sigma**2)
+                    reserve_spread = (2 / self.gamma) * np.log(1 +
+                                                            self.gamma / self.k)
+
+                    r_optimal_ask[step] = reserve_price[step] + reserve_spread / 2
+                    r_optimal_bid[step] = reserve_price[step] - reserve_spread / 2
+                    optimal_distance_ask = -self.gamma * orders[step] * (
+                        self.sigma**
+                        2) + (1 / self.gamma) * np.log(1 + (self.gamma / self.k))
+                    optimal_distance_bid = self.gamma * orders[step] * (
+                        self.sigma**
+                        2) + (1 / self.gamma) * np.log(1 + (self.gamma / self.k))
+
+                    lambda_ask = np.exp(-self.k * optimal_distance_ask)
+                    lambda_bid = np.exp(-self.k * optimal_distance_bid)
+
+                    ask_probability = 1 - math.exp(-lambda_ask)
+                    bid_probability = 1 - math.exp(-lambda_bid)
+
+                    ask_amount = 0
+                    bid_amount = 0
+                    if random.random() < ask_probability:
+                        ask_amount = 1
+                    if random.random() < bid_probability:
+                        bid_amount = 1
+                    
+                    orders[step + 1] = orders[step] - ask_amount + bid_amount
+                    cashflow[step + 1] = cashflow[step] + r_optimal_ask[
+                        step] * ask_amount - r_optimal_bid[step] * bid_amount
+                    pnl[step +
+                        1] = cashflow[step + 1] + orders[step + 1] * prices[step]
+
+                pnl_of_simulation[simulation] = pnl[-1]
+                final_inventory[simulation] = orders[-1]
+
+            self.orders = orders
+            self.cashflow = cashflow
+            self.pnl = pnl
+            self.bidPrice = r_optimal_bid
+            self.askPrice = r_optimal_ask
+            for prediction in bid_probability:
+                    # 根据预测结果生成信号
+                    if random.random() < prediction:
+
+                        signal = {
+                            "symbol": "AAPL",  # 股票代码
+                            "direction": "B",  # 买入 
+                            "bidprice": r_optimal_bid,  # 买价
+                            "volume": orders  # 交易量
+                        }
+                    else:
+                        signal = {
+                            "symbol": "AAPL",  # 股票代码
+                            "direction": "S",  # 卖出 
+                            "askprice": r_optimal_ask,  #卖价
+                            "volume": orders  # 交易量
+                        }
+                    self.signals.append(signal)
+
+            return self.signals
+        
+        def signal_update(deals: Dict[int, deal], portfolio: Dict[str, possession], 
+                          timestamp: int, symbol: str, direction: str, price: float, volume: int):
+            deals[timestamp] = deal(timestamp, symbol, direction, price, volume)
+
+            # 更新持仓记录
+            if symbol in portfolio:
+                # 如果股票代码已存在于持仓记录中，更新相应的数量、平均价格和成本
+                possession = portfolio[symbol]
+                if direction == "B":
+                    possession.volume += volume
+                    possession.cost += price * volume
+                    possession.averagePrice = possession.cost / possession.volume
+                elif direction == "S":
+                    possession.volume -= volume
+                    possession.cost -= price * volume
+                    if possession.volume == 0:
+                        # 如果持仓数量为0，从持仓记录中移除该股票代码
+                        del portfolio[symbol]
+                else:
+                    # 处理未知的交易方向
+                    print("Unknown direction:", direction)
             else:
-                deltaBid[t] = self.deltaBidZero
-                deltaAsk[t] = self.deltaAskZero
+                # 如果股票代码不存在于持仓记录中，创建新的持仓记录
+                if direction == "B":
+                    cost = price * volume
+                    averagePrice = price
+                    possession = possession(symbol, volume, averagePrice, cost)
+                    portfolio[symbol] = possession
+                elif direction == "S":
+                    # 处理卖出时没有对应的买入记录的情况
+                    print("No corresponding buy record for sell transaction:", symbol)
+                else:
+                    # 处理未知的交易方向
+                    print("Unknown direction:", direction)
 
-        for simulation in range(self.number):
-            pnl = np.zeros((len(prices)))
-            cashflow = np.zeros((len(prices)))
-            orders = np.zeros((len(prices)))
-            reserve_price = np.zeros((len(prices)))
-            r_optimal_ask = np.zeros((len(prices)))
-            r_optimal_bid = np.zeros((len(prices)))
+        def strategy_update(deal: Dict[int, deal]):
+            """
+            根据更新过后的signal, 更新成交记录和持仓记录, 更新策略的评价结果
+            评价指标是收益率
+            """
+            total_profit = {}
+            # 遍历成交记录，计算每笔交易的收益率
+            for timestamp, deal in deal.items():
+                symbol = deal.symbol
+                direction = deal.direction
+                price = deal.price
+                volume = deal.volume
 
-            for step in range(len(prices) - 1):
-                reserve_price[step] = prices[
-                    step] - orders[step] * self.gamma * (self.sigma**2)
-                reserve_spread = (2 / self.gamma) * np.log(1 +
-                                                           self.gamma / self.k)
+                if direction == "B":
+                    # 如果是买入交易，记录买入价格
+                    total_profit[timestamp] = {
+                        "symbol": symbol,
+                        "buy_price": price,
+                        "sell_price": None,
+                        "returns": None
+                    }
+                elif direction == "S":
+                    # 如果是卖出交易，计算收益率并更新字典
+                    if timestamp in total_profit:
+                        total_profit[timestamp]["sell_price"] = price
+                        buy_price = total_profit[timestamp]["buy_price"]
+                        sell_price = total_profit[timestamp]["sell_price"]
+                        total_profit[timestamp]["returns"] = (sell_price - buy_price) / buy_price
 
-                r_optimal_ask[step] = reserve_price[step] + reserve_spread / 2
-                r_optimal_bid[step] = reserve_price[step] - reserve_spread / 2
-                optimal_distance_ask = -self.gamma * orders[step] * (
-                    self.sigma**
-                    2) + (1 / self.gamma) * np.log(1 + (self.gamma / self.k))
-                optimal_distance_bid = self.gamma * orders[step] * (
-                    self.sigma**
-                    2) + (1 / self.gamma) * np.log(1 + (self.gamma / self.k))
+            return total_profit
+        # signal_result = signal_update(deal, possession, self.T, "AAPL", "B", self.prices, self.orders)#还需要改
+        # self.print_signal_result(signal_result)
+        strategy_result = strategy_update(deal)
+        self.print_strategy_result(strategy_result)
 
-                lambda_ask = np.exp(-self.k * optimal_distance_ask)
-                lambda_bid = np.exp(-self.k * optimal_distance_bid)
+    def print_signals(self):
+        # 输出信号列表
+        signals = self.signals
+        for signal in signals:
+            print(signal)
 
-                ask_probability = 1 - math.exp(-lambda_ask)
-                bid_probability = 1 - math.exp(-lambda_bid)
+    def print_positions(portfolio: Dict[str, possession]):
+        # 输出持仓记录
+        for symbol, possession in portfolio.items():
+            print(f"Stock Code: {symbol}")
+            print(f"Volume: {possession.volume}")
+            print(f"Average Price: {possession.averagePrice}")
+            print(f"Cost: {possession.cost}")
+            print()
 
-                ask_amount = 0
-                bid_amount = 0
-                if random.random() < ask_probability:
-                    ask_amount = 1
-                if random.random() < bid_probability:
-                    bid_amount = 1
-                
-                orders[step + 1] = orders[step] - ask_amount + bid_amount
-                cashflow[step + 1] = cashflow[step] + r_optimal_ask[
-                    step] * ask_amount - r_optimal_bid[step] * bid_amount
-                pnl[step +
-                    1] = cashflow[step + 1] + orders[step + 1] * prices[step]
+    def print_transactions(deal: Dict[int, deal]):
+        # 输出成交记录
+        for timestamp, deal in deal.items():
+            print(f"Timestamp: {timestamp}")
+            print(f"Symbol: {deal['symbol']}")
+            print(f"Direction: {deal['direction']}")
+            print(f"Price: {deal['price']}")
+            print(f"Volume: {deal['volume']}")
+            print()
+    
+    def print_strategy_result(self, result: Dict):
+        # 输出策略的评价结果
+        print(result)
 
-            pnl_of_simulation[simulation] = pnl[-1]
-            final_inventory[simulation] = orders[-1]
-
-        self.orders = orders
-        self.cashflow = cashflow
-        self.pnl = pnl
-        self.bidPrice = r_optimal_bid
-        self.askPrice = r_optimal_ask
-
-        # 调用model_update函数，更新模型
-        self.model_update(self,bid_probability)
-
-        # 调用signal_update函数，更新信号
-        self.signal_update()
-
-        # 调用strategy_update函数，更新策略
-        self.strategy_update()
+         
 
 
-        return pnl_of_simulation, final_inventory
+         
 
     # def getPrices(self):
     #     """
