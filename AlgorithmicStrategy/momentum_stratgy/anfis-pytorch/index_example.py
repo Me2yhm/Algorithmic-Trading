e241750e8c3f42df3daeabdd6e8eec27a5ba606f
echo Yang <Yhmecnu@163.com>
2023-08-31 14:04:50 +0800

    fix: change the shape of input

--- AlgorithmicStrategy/momentum_stratgy/anfis-pytorch/index_example.py
+++ AlgorithmicStrategy/momentum_stratgy/anfis-pytorch/index_example.py
@@ -10,7 +10,6 @@ from pathlib import Path
 import torch
 from torch.utils.data import TensorDataset, DataLoader
 
-import anfis
 from membership import BellMembFunc, make_bell_mfs, make_anfis
 from experimental import train_anfis, test_anfis
 
@@ -18,11 +17,11 @@ from experimental import train_anfis, test_anfis
 def make_train_data(
     file_path: str,
     batch_size=1024,
-):
+) -> DataLoader:
     data = pd.read_csv(file_path, header=None)
-    *x, y = [list(data.values[:, i] for i in (0, 1, 2))]
-    x = torch.tensor(x)
-    y = torch.tensor(y)
+    *x, y = [list(data.values[:, i]) for i in range(3)]
+    x = torch.tensor(x).transpose(-1, 0)
+    y = torch.tensor(y).transpose(-1, 0)
     td = TensorDataset(x, y)
     return DataLoader(td, batch_size=batch_size, shuffle=True)
 
@@ -32,8 +31,8 @@ def make_model(data, num_mfs, num_out):
     return model
 
 
-if __name__ == "__mian__":
-    file_path = r"AlgorithmicStrategy\momentum_stratgy\anfis-pytorch\index_example.py"
+if __name__ == "__main__":
+    file_path = r"AlgorithmicStrategy\momentum_stratgy\anfis-pytorch\index_marked.csv"
     data = make_train_data(file_path=file_path)
     x, _ = data.dataset.tensors
     model = make_model(x, num_mfs=2, num_out=1)
